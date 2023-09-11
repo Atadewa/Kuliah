204f96bec454fdb5a0a2d409dc71a008f79a7c45
Atadewa <aditafli42@gmail.com>
2023-09-11 12:42:23 +0700

    commit

---
+++ Week 1/MyFirstJava03.java
@@ -0,0 +1,6 @@
+public class MyFirstJava03{
+    public static void main(String[] args) {
+        
+        System.out.println("Nama saya Aditya Atadewa");
+    }
+}
---
+++ Week 1/Tugas03.java
@@ -0,0 +1,13 @@
+/**
+ * Tugas03
+ */
+public class Tugas03 {
+    public static void main(String[] args) {
+        System.out.print ("***** IDENTITAS DIRI *****\n");
+        System.out.println ("Nama            : Aditya Atadewa");
+        System.out.println ("Absen           : 03");
+        System.out.println ("Kelas           : 1A");
+        System.out.println ("Prodi           : D-IV Teknik Informatika");
+        System.out.println ("Jurusan         : Teknologi Informasi");
+    }
+}
