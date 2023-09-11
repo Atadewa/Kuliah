6d3743d1d8d6e7bb5eb564bedd04f1d9255ca611
Atadewa <aditafli42@gmail.com>
2023-09-11 15:16:05 +0700

    commit

--- Semester 1/Praktikum Daspro/Week 3/ContohVariabel03.java
+++ Semester 1/Praktikum Daspro/Week 3/ContohVariabel03.java
@@ -2,13 +2,13 @@ public class ContohVariabel03 {
     public static void main(String[] args) {
         String hobbySaya = "Bermain petak umpet";
         boolean isPandai = true;
-        char jenisKelamin = 'L';
-        byte umurSekarang = 20;
-        double $ipk = 3.24, tinggi = 1.78;
+        char gender = 'L';
+        byte umur = 20;
+        double ipk = 3.24, tinggi = 1.78;
         System.out.println(hobbySaya);
         System.out.println("Apakah pandai? " + isPandai);
-        System.out.println("Jenis kelamin : " + jenisKelamin);
-        System.out.println("Umurku saat ini : " + umurSekarang);
-        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+        System.out.println("Jenis kelamin : " + gender);
+        System.out.println("Umurku saat ini : " + umur);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", ipk, tinggi));
     }
 }
