d8ab76dc95b2ec660c8a39997626d07f16a4bcc0
Atadewa <aditafli42@gmail.com>
2023-09-25 10:12:21 +0700

    modify java file

--- Semester 1/Praktikum Daspro/Week 5/PemilihanPercobaan203.java
+++ Semester 1/Praktikum Daspro/Week 5/PemilihanPercobaan203.java
@@ -13,10 +13,38 @@ public class PemilihanPercobaan203 {
         float tugas = input03.nextFloat();
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+
+        String nilaiHuruf = null, kualifikasi = null; 
+
+        if (total <= 39) {
+            nilaiHuruf = "E";
+            kualifikasi = "Gagal";
+        } else if (total <= 50) {
+            nilaiHuruf = "D";
+            kualifikasi = "Kurang";
+        } else if (total <= 60) {
+            nilaiHuruf = "C";
+            kualifikasi = "Cukup";
+        } else if (total <= 65) {
+            nilaiHuruf = "C+";
+            kualifikasi = "Lebih dari Cukup";
+        } else if (total <= 73) {
+            nilaiHuruf = "B";
+            kualifikasi = "Baik";
+        } else if (total <= 80) {
+            nilaiHuruf = "B+";
+            kualifikasi = "Lebih dari Baik";
+        } else if (total <= 100) {
+            nilaiHuruf = "A";
+            kualifikasi = "Sangat Baik";
+        }
+        
         String message = total < 65 ? "Remidi" : "Tidak remidi";
-
-        System.out.println("Nilai akhir = " + total + " sehingga " + message);
-
+        
+        System.out.println("\nNilai akhir\t= "+total);
+        System.out.println("Nilai huruf\t= "+nilaiHuruf);
+        System.out.println("Kualifikasi \t= "+kualifikasi);
+        System.out.println("Hasil\t\t= "+message);
     } 
     
 }
