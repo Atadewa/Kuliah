69c2928ff4efcf6aae4caa0ec6be12eb25335976
Atadewa <aditafli42@gmail.com>
2023-10-02 13:45:39 +0700

    modify again

--- Semester 1/Praktikum Daspro/Week 6/Pemilihan2Percobaan103.java
+++ Semester 1/Praktikum Daspro/Week 6/Pemilihan2Percobaan103.java
@@ -6,7 +6,9 @@ public class Pemilihan2Percobaan103 {
         int tahun = input03.nextInt();
         
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
+            if ((tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+            } else if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
             } else {
                 System.out.println("Bukan Tahun Kabisat");
