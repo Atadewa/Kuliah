ed0f3d286a0f93c70ce9bf7e19ae0afefddc3457
Atadewa <aditafli42@gmail.com>
2023-10-09 11:14:38 +0700

    modify

--- Semester 1/Praktikum Daspro/Week 7/ForKelingan03.java
+++ Semester 1/Praktikum Daspro/Week 7/ForKelingan03.java
@@ -3,10 +3,10 @@ public class ForKelingan03 {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
         int kelipatan, jumlah=0, counter=0;
+        double average;
         
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
-
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
                 jumlah += i;
@@ -14,8 +14,11 @@ public class ForKelingan03 {
             }
         }
 
+        average = jumlah/counter;
+        
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata dari bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, average);
 
     }
 }
