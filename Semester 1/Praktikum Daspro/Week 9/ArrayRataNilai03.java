99fa9cc48a756930270a25c8b8f9d01896177b91
Atadewa <aditafli42@gmail.com>
2023-10-25 11:20:06 +0700

    modify

--- Semester 1/Praktikum Daspro/Week 9/ArrayRataNilai03.java
+++ Semester 1/Praktikum Daspro/Week 9/ArrayRataNilai03.java
@@ -2,21 +2,34 @@ import java.util.Scanner;
 public class ArrayRataNilai03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int[] nilaiMhs = new int [10];
-        double total = 0;
-        double rata2;
+
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jmlMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int [jmlMhs];
+        double totalLulus = 0, totalTdkLulus = 0;
+        double rata2Lulus = 0, rata2TdkLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
 
+        int counterLulus = 0, counterTdkLulus = 0;
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                counterLulus++;
+            } else {
+                totalTdkLulus += nilaiMhs[i];
+                counterTdkLulus++;
+            }
         }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        rata2Lulus = totalLulus/counterLulus;
+        rata2TdkLulus = totalTdkLulus/counterTdkLulus;
+        System.out.println("Rata-rata nilai lulus = "+rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+rata2TdkLulus);
 
     }
 }
