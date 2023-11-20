105d6fc4fa7eee21d5fba67018592a53f1fb43b0
Atadewa <aditafli42@gmail.com>
2023-11-20 13:29:11 +0700

    modify

--- Semester 1/Praktikum Daspro/Week 13/TerimaKasih_03.java
+++ Semester 1/Praktikum Daspro/Week 13/TerimaKasih_03.java
@@ -3,8 +3,13 @@ public class TerimaKasih_03 {
         System.out.println("Thank you for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
-    
+
+    public static void UcapanTambahan (String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = "Terimakasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
