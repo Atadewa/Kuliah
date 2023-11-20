3287753a871ff6f4536ad5b31567cedb9bcb8d06
Atadewa <aditafli42@gmail.com>
2023-11-20 13:38:18 +0700

    add UcapanTerimaKasih.java

--- Semester 1/Praktikum Daspro/Week 13/UcapanTerimaKasih_03.java
+++ Semester 1/Praktikum Daspro/Week 13/UcapanTerimaKasih_03.java
@@ -9,9 +9,13 @@ public class UcapanTerimaKasih_03 {
         return namaOrang;
     }
 
-    public static void main(String[] args) {
+    public static void UcapanTerimaKasih() {
         String nama = PenerimaUcapan();
         System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
-        "You inspired in me a love for learning and made me feel like I could ask you anything.\"");
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
     }
 }
