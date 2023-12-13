03b5a0d3243bfed5eca9b41238bcd598008a9b40
Atadewa <aditafli42@gmail.com>
2023-12-13 18:13:12 +0700

    feat: add tugas.java

---
+++ Semester 1/Praktikum Daspro/Week 14/Tugas203.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Tugas203 {
+
+    static int penjumlahanRekursif (int x, int bil) {
+        if (x == bil) {
+            System.out.print(bil + " = ");
+            return bil;
+        } else {
+            System.out.print(x + "+");
+            return (x + penjumlahanRekursif(x + 1, bil));
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int angka;
+
+        System.out.print("Masukkan angka : ");
+        angka = sc.nextInt();
+        
+        System.out.println(penjumlahanRekursif(1, angka));
+    }
+
+}
