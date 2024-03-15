67fccb7b7fdbc5c2f0c6e6f15565e43d4ba57512
Atadewa <aditafli42@gmail.com>
2024-03-15 10:21:08 +0700

    modify

--- Semester 2/Praktikum Algoritma dan Struktur Data/Week 3/src/ArrayObjects.java
+++ Semester 2/Praktikum Algoritma dan Struktur Data/Week 3/src/ArrayObjects.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
 
-class PersegiPanjang {
-    public int panjang;
-    public int lebar;
-}
-
 public class ArrayObjects {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         
         System.out.print("Masukkan panjang array : ");
         int jml = sc.nextInt();
-
+        
         PersegiPanjang[] ppArray = new PersegiPanjang[jml];
-
+        
         for (int i = 0; i < ppArray.length; i++) {
             ppArray[i] = new PersegiPanjang();
-            System.out.println("\nPersegi panjang ke-" + i);
+            System.out.println("\nPersegi panjang ke-" + (i+1));
             System.out.print("Masukkan panjang: ");
             ppArray[i].panjang = sc.nextInt();
             System.out.print("Masukkan lebar: ");
             ppArray[i].lebar = sc.nextInt();
         }
 
+        System.out.println("\n=======================");
+
         for (int i = 0; i < ppArray.length; i++) {
-            System.out.println("\nPersegi Panjang ke-" + i);
+            System.out.println("\nPersegi Panjang ke-" + (i+1));
             System.out.println("Panjang: " + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
         }
     }
