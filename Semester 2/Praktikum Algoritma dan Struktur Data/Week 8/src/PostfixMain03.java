906cb578b9cea67f0601188f941f7a5aa0bfbf88
Atadewa <aditafli42@gmail.com>
2024-04-18 13:23:23 +0700

    add infix to postfix program

---
+++ Semester 2/Praktikum Algoritma dan Struktur Data/Week 8/src/PostfixMain03.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PostfixMain03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String P, Q;
+
+        System.out.println("Masukkan ekspresi matematika (infix): ");
+        Q = sc.nextLine();
+        Q = Q.trim();
+        Q = Q + ')';
+
+        int total = Q.length();
+        Postfix03 post = new Postfix03(total);
+        P = post.konversi(Q);
+
+        System.out.println("\nPostfix: " + P);
+    }
+}
