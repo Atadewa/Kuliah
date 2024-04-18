21dad18226d0f01145f72dda5e9742516fa2e6a8
Atadewa <aditafli42@gmail.com>
2024-04-18 13:21:37 +0700

    add Barang03.java

---
+++ Semester 2/Praktikum Algoritma dan Struktur Data/Week 8/src/Barang03.java
@@ -0,0 +1,11 @@
+public class Barang03 {
+    int kode;
+    String nama;
+    String kategori;
+
+    Barang03 (int kode, String nama, String kategori) {
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }  
+}
